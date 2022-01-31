3684dc41b7d85a571f1ea6004b6e752da27061db
kmingki <wl1524@naver.com>
2022-01-31 16:03:48 +0900

    :sparkles: feat #1 : recoil root 전역 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Router from './router';
+import {
+  RecoilRoot,
+} from 'recoil';
 
 const App = () => {
   return (
     <>
+    <RecoilRoot>
       <Router />
+    </RecoilRoot>
     </>
   );
 };
