1efb2dfb85c824177a4e657f0e04185c12935c1c
minsgy <alstjrchl97@likelion.org>
2022-02-09 22:24:11 +0900

    🎨 style #17 : Global emotion react 지정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Global } from '@emotion/core';
+import { Global } from '@emotion/react';
 import { globalStyle } from '@styles';
 import Router from '@router';
 const App = () => {
