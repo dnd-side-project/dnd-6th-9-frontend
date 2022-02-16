17c4cd1d9f3e1048c67cb041451b7bd28d7e7642
minsgy <alstjrchl97@likelion.org>
2022-02-17 02:07:54 +0900

    ✨ feat #45 : Input Refactoring 변경 사항 스토리북 적용

--- src/stories/components/Input.stories.js
+++ src/stories/components/Input.stories.js
@@ -1,14 +1,31 @@
 import { Input } from '@components/base';
-
+import { useState } from 'react';
 export default {
   title: 'Components/Input',
   component: Input,
 };
 
 export const Default = (...args) => {
-  return <Input {...args} />;
+  const [error, setError] = useState(false);
+  const handleChange = ({ value }) => {
+    if (value) console.log(value);
+  };
+  const handleError = () => {
+    setError((prev) => !prev);
+  };
+
+  return (
+    <>
+      <button onClick={handleError}>Click</button>
+      <Input {...args} onChange={handleChange} maxLength={10} error={error} />
+    </>
+  );
 };
 
 export const Error = (...args) => {
-  return <Input {...args} error />;
+  return (
+    <>
+      <Input {...args} error />
+    </>
+  );
 };
