5fc667bec0a2ab581d93574e67e25b4d2404a93c
minsgy <alstjrchl97@likelion.org>
2022-02-16 16:24:42 +0900

    ✨ feat #58 : DefaultTemplate 추가 (상속받아 사용하기)

--- src/presentation/styles/index.jsx
+++ src/presentation/styles/index.jsx
@@ -2,3 +2,4 @@ export { default as globalStyle } from './global';
 export { default as FONT } from './fonts';
 export { default as COLOR } from './colors';
 export { default as theme } from './theme';
+export { default as DefaultTemplate } from './templates/DefaultTemplate';
