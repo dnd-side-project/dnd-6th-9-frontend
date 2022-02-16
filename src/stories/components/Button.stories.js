eb28dc06ea1056856af6ce4ff69bfb660a4fe926
kmingki <wl1524@naver.com>
2022-02-16 12:49:27 +0900

    ✨ feat #16 : Button 컴포넌트 구현 및 storybook 제작

---
+++ src/stories/components/Button.stories.js
@@ -0,0 +1,54 @@
+import { Button } from '@components/base';
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+};
+
+export const Default = () => {
+  return (
+    <>
+      <Button type={'primary'} size={'m'} disabled={false}>
+        Button
+      </Button>
+
+      <Button type={'primary'} size={'l'} disabled={false}>
+        Button
+      </Button>
+
+      <Button type={'primary'} size={'l'} disabled={true}>
+        Button
+      </Button>
+
+      <Button type={'secondary'} size={'m'} disabled={false}>
+        Button
+      </Button>
+      <Button type={'secondary'} size={'l'} disabled={false}>
+        Button
+      </Button>
+      <Button type={'secondary'} size={'l'} disabled={true}>
+        Button
+      </Button>
+
+      <Button type={'danger'} size={'m'} disabled={false}>
+        Button
+      </Button>
+      <Button type={'danger'} size={'l'} disabled={false}>
+        Button
+      </Button>
+      <Button type={'danger'} size={'l'} disabled={true}>
+        Button
+      </Button>
+
+      <Button type={'select'} size={'m'} disabled={false}>
+        Button
+      </Button>
+      <Button type={'select'} size={'l'} disabled={false}>
+        Button
+      </Button>
+      <Button type={'select'} size={'l'} disabled={true}>
+        Button
+      </Button>
+    </>
+  );
+};
