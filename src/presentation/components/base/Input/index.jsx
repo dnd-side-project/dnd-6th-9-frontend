12f26eef5a2826ea44da0615782c19acbb71df7f
minsgy <alstjrchl97@likelion.org>
2022-02-17 02:08:36 +0900

    ✨ feat #45 : Input 컴포넌트 리팩토링 완료

--- src/presentation/components/base/Input/index.jsx
+++ src/presentation/components/base/Input/index.jsx
@@ -1,16 +1,51 @@
-import React from 'react';
-import InputRoot from './style';
+import React, { useState } from 'react';
+import { textCheckByte } from '@utils/validateFunc';
+import { RootInput, RootWrapper, CountText, ErrorIcon } from './style';
 import PropTypes from 'prop-types';
 
-const Input = ({ width, onChange, placeholder, error, ...props }) => {
+const Input = ({
+  width,
+  onChange,
+  placeholder,
+  error,
+  maxLength,
+  ...props
+}) => {
+  const [value, setValue] = useState('');
+  const [isFocus, setIsFocus] = useState(false);
+  const handleChange = (e) => {
+    const { value } = e.target;
+    const { isSuccess } = textCheckByte(value, maxLength);
+    if (isSuccess) {
+      setValue(value);
+      onChange && onChange({ value });
+    }
+  };
+
+  const handleFocus = (e) => {
+    const { type } = e;
+    type === 'focus' ? setIsFocus(true) : setIsFocus(false);
+  };
+
   return (
-    <InputRoot
-      width={width}
-      onChange={onChange}
-      className={error ? 'error' : ''}
-      placeholder={placeholder}
-      {...props}
-    />
+    <RootWrapper value={value} isFocus={isFocus} error={error}>
+      <RootInput
+        width={width}
+        value={value}
+        onChange={handleChange}
+        onFocus={handleFocus}
+        onBlur={handleFocus}
+        className={error ? 'error' : ''}
+        placeholder={placeholder}
+        {...props}
+      />
+      {error && <ErrorIcon />}
+      {maxLength && (
+        <CountText>
+          {value.length}/{maxLength}
+        </CountText>
+      )}
+    </RootWrapper>
   );
 };
 
@@ -20,7 +55,8 @@ Input.propTypes = {
   width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   onChange: PropTypes.func,
-  error: PropTypes.boolean,
+  error: PropTypes.bool,
+  maxLength: PropTypes.number,
   placeholder: PropTypes.string,
 };
 
--- src/presentation/components/base/Input/style.js
+++ src/presentation/components/base/Input/style.js
@@ -1,28 +1,42 @@
 import styled from '@emotion/styled';
+import { ReactComponent as Error_Icon } from '@assets/icons/error_icon.svg';
 import { typeOfSize } from '@utils/conversionsFunc';
-import { COLOR } from '@styles';
+import { COLOR, FONT } from '@styles';
+
+const RootWrapper = styled.div`
+  ${({ theme }) => theme.common.flexCenter}
+  width: ${({ width }) => typeOfSize(width)};
+  height: ${({ height }) => typeOfSize(height)};
+  color: ${COLOR.GRAY_500};
+  border-bottom: 1px solid ${COLOR.GRAY_500};
+  ${({ value, isFocus }) =>
+    (value.length || (isFocus && value.length === 0)) &&
+    ` 
+       color: ${COLOR.GRAY_900};
+       border-bottom: 1px solid ${COLOR.GRAY_900};
+    `}
+  ${({ error }) => error && `border-bottom: 1px solid ${COLOR.RED_500}`}
+`;
 
 const RootInput = styled.input`
   width: ${({ width }) => typeOfSize(width)};
   height: ${({ height }) => typeOfSize(height)};
   padding: 0 8px;
-  color: ${COLOR.GRAY_800};
   border: none;
   box-sizing: border-box;
-  border-bottom: 1px solid ${COLOR.GRAY_500};
-  &:placeholder-shown {
-    color: ${COLOR.GRAY_500};
-    border-bottom: 1px solid ${COLOR.GRAY_300};
-  }
-  &:focus {
-    color: ${COLOR.GRAY_900};
-    outline: none;
-    border-bottom: 1px solid ${COLOR.GRAY_900};
-  }
-  &.error {
-    color: ${COLOR.GRAY_800};
-    border-bottom: 1px solid ${COLOR.RED_500};
-  }
+  ${FONT.R_14_BODY}
 `;
 
-export default RootInput;
+const ErrorIcon = styled(Error_Icon)`
+  width: 20px;
+  height: 18px;
+  margin-right: 8px;
+`;
+
+const CountText = styled.p`
+  color: ${COLOR.GRAY_500};
+  margin-right: 10px;
+  ${FONT.R_14_BODY}
+`;
+
+export { RootInput, RootWrapper, CountText, ErrorIcon };
