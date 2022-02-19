965b29735d999e7dee6f3cc3794e11fd6e9af013
kmingki <wl1524@naver.com>
2022-02-19 15:13:26 +0900

    ✨ feat #63 : Message 생성페이지 구현

--- src/presentation/router/index.jsx
+++ src/presentation/router/index.jsx
@@ -1,10 +1,10 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import { MessageCreatePage } from '@pages';
+
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MessageCreatePage />} />
+        <Route path="/" element={<div>test</div>} />
       </Routes>
     </BrowserRouter>
   );
