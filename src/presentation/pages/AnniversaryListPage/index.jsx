7e2d3d0fa7da83cbe451b197e58f6e6d33e3426a
kmingki <wl1524@naver.com>
2022-03-01 11:43:36 +0900

    ✨ feat #91 : tab 컴포넌트 수정

---
+++ src/presentation/pages/AnniversaryListPage/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { DefaultPageHeader } from '@components/domain';
+
+const AnniversaryListPage = () => {
+  const handleGoBack = () => {};
+
+  return <DefaultPageHeader handleGoBack={handleGoBack} />;
+};
+
+export default AnniversaryListPage;
